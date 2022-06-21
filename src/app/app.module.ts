3d7cfe1aa7ac7f62a98bd6afb697dcdbe1f41cf7
unknown <rwps1234@gmail.com>
2022-06-21 14:40:43 -0300

    importando FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { GithubService } from './github.service';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, RepositoriesComponent],
+  imports: [BrowserModule, HttpClientModule,FormsModule],
+  providers: [HttpClientModule,GithubService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
