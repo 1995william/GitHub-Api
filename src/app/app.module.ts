8d7aa72c6eca753c677b557987af40c93242ade6
unknown <rwps1234@gmail.com>
2022-06-22 01:11:43 -0300

    importando header no NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { GithubService } from './github.service';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [AppComponent, RepositoriesComponent],
+  declarations: [AppComponent, RepositoriesComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule,FormsModule],
   providers: [HttpClientModule,GithubService],
   bootstrap: [AppComponent],
