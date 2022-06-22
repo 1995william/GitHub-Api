16d9c87b5adf634c72a32fff33989fefe042d3ab
unknown <rwps1234@gmail.com>
2022-06-22 01:10:30 -0300

    criando metodo next e previous

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -4,28 +4,38 @@ import { GithubService } from '../github.service';
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  styleUrls: ['./repositories.component.css'],
 })
 export class RepositoriesComponent implements OnInit {
-  
   totalResults: string | undefined;
   repositories: Array<any> = new Array();
-
   inputValue: string | undefined;
+  count: number = 1;
 
-  constructor(private githubService: GithubService) { }
+  constructor(private githubService: GithubService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  previous() {
+    if (this.count > 1) {
+      this.count--;
+      this.listRepositories();
+    }
   }
 
-    listRepositories () {
-      this.githubService.getData(this.inputValue).subscribe(repo => {
-        this.totalResults = `${repo.total_count} Resultados`
+  next() {
+    this.count++;
+    this.listRepositories();
+  }
+
+  listRepositories() {
+    this.githubService
+      .getData(this.inputValue, this.count)
+      .subscribe((repo) => {
+        this.totalResults = `${repo.total_count} Resultados`;
         this.repositories = repo.items;
 
-        console.log(typeof this.totalResults)
-        console.log(this.repositories)
-
-      })
-    }
+        console.log(this.repositories);
+      });
+  }
 }
