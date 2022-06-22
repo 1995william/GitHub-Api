4b87cd163e48ffc0dda2d9e8dfdd5474fcbf3e49
unknown <rwps1234@gmail.com>
2022-06-22 19:11:19 -0300

    inserindo testes unitários

--- src/app/github.service.spec.ts
+++ src/app/github.service.spec.ts
@@ -1,16 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { GithubService } from './github.service';
 
-describe('GithubService', () => {
+
+fdescribe('GithubService', () => {
   let service: GithubService;
+  let http: HttpClient;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(GithubService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('ele deve chamar um GET com endpoint correto', () =>{
+    const spy = spyOn((service as any).http as HttpClient, 'get');
+    service.getData('',1)
+    expect(spy).toHaveBeenCalled()
+  })
 });
